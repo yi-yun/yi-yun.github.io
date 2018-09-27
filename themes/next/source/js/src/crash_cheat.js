9624657933fb6ca18bdcc27c439f9f7df62a0aa2
yi-yun <tp91225@163.com>
2018-09-27 21:59:05 +0800

    modify title

--- themes/next/source/js/src/crash_cheat.js
+++ themes/next/source/js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/images/TEP.png");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         document.title = '╭(°A°`)╮ 改需求的来了 ~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/images/favicon-16x16-next.png");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         document.title = '(ฅ>ω<*ฅ) 噫又跑了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
